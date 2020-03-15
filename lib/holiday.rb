93237d922c57d4c54b7928fdc98ef8c48db63fec
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-15 14:46:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holidays|
     holidays.map do |event, supplies|
-      if supplies.includes?("BBQ")
+      if supplies.include?("BBQ")
         event
       end
     end
